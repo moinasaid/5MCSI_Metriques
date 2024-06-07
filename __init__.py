59e2428d7fae42d374f7b1e790a518e8a2a93c10
moinasaid <167031063+moinasaid@users.noreply.github.com>
2024-06-07 11:04:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+    
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -20,5 +21,6 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+    
 if __name__ == "__main__":
     app.run(debug=True)
