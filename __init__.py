6da8e48ca0667cf7d0900d8e922810de00900d2a
moinasaid <167031063+moinasaid@users.noreply.github.com>
2024-06-07 11:59:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,10 @@ def mongraphique():
 def monhistogramme():
     return render_template("Histogramme.html")
 
+@app.route("/moncontact/")
+def MaPremiereAPI():
+    return render_template("moncontact.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
 
