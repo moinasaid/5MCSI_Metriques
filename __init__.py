2f1429499b1b45feda2f45e5822f5a2adc2eae25
moinasaid <167031063+moinasaid@users.noreply.github.com>
2024-06-07 12:34:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -41,7 +41,7 @@ def MaPremiereAPI():
 
 @app.route('/commits/data')
 def get_commits_data():
-    url = 'https://api.github.com/repos/OliveurTwistAgain/Flask_Meteo/commits'
+    url = 'https://api.github.com/repos/moinasaid/5MCSI_Metriques/commits'
     response = urlopen(url)
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
