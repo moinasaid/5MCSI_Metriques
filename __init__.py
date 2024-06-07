50f9cbe7f16ed16f68d3f6f8a22d28bf79485996
moinasaid <167031063+moinasaid@users.noreply.github.com>
2024-06-07 10:46:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') 
-  @app.route("/contact/")
+    return render_template('hello.html')
+
+@app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
